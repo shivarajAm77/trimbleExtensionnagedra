2279f6a070d074373dfd5ba8d83ef6141e145a76
shivarajAm77 <137749471+shivarajAm77@users.noreply.github.com>
2026-05-12 17:06:06 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
     onLoginSuccess(window.keycloak.tokenParsed);
     document.getElementById("logoutBtn").style.display = "inline-block";
-    
+    console.log("standaloneor not" , window.self === window.top);
  if (window.self === window.top) {
       console.log("Closing standalone tab");
       window.close();
